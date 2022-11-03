f1115afaa2be98558c6b4717be99496921b4f662
Aram <aramik16spartak@mail.ru>
2022-11-03 23:21:53 +0300

    Empty project

---
+++ precache-manifest.ef2329c1db9cf11eb9a518637cb3d2be.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "97e413a26708de84784d",
+    "url": "/css/app.30d522ac.css"
+  },
+  {
+    "revision": "97e413a26708de84784d",
+    "url": "/js/app.a79736a6.js"
+  },
+  {
+    "revision": "e177541b567a978840f9",
+    "url": "/js/chunk-23d7a810.5a6eeee3.js"
+  },
+  {
+    "revision": "4e5aa0768c36ae21ea40",
+    "url": "/js/chunk-2d0c1056.fb688f41.js"
+  },
+  {
+    "revision": "a7f21fdce2446343ebe1",
+    "url": "/js/chunk-2d0c9708.05c3452d.js"
+  },
+  {
+    "revision": "6a5aeae7c9119d5efabb",
+    "url": "/js/chunk-2d0d05dc.db312efc.js"
+  },
+  {
+    "revision": "7534387603655e3d2ad8",
+    "url": "/js/chunk-2d217357.605c492e.js"
+  },
+  {
+    "revision": "de35dfdfbeb79736ade7",
+    "url": "/js/chunk-2d217a99.9e628112.js"
+  },
+  {
+    "revision": "5bda59a216e3a5aff922",
+    "url": "/js/chunk-2d21a3d2.148e38f5.js"
+  },
+  {
+    "revision": "aee06dee25df5d1ddb3a",
+    "url": "/js/chunk-2d2259e5.60395287.js"
+  },
+  {
+    "revision": "b3cbd63ed24619c777b7",
+    "url": "/js/chunk-37f41dab.1f38cc92.js"
+  },
+  {
+    "revision": "694dbc2e7a6528388bab",
+    "url": "/css/chunk-vendors.9704f4e7.css"
+  },
+  {
+    "revision": "694dbc2e7a6528388bab",
+    "url": "/js/chunk-vendors.26697e71.js"
+  },
+  {
+    "revision": "aa1c00216db33480262032cf1cd69758",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
